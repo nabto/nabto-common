995491fb96391f7aa2c114b826342149afe48e93
Michael <mkm@nabto.com>
2024-07-31 14:34:24 +0200

    Created two examples which uses the nn library

---
+++ test/distribution/fetch_content/README.md
@@ -0,0 +1,7 @@
+# usage
+
+```
+cmake -DCMAKE_INSTALL_PREFIX=install <path to this folder>
+make
+./test
+```
---
+++ test/distribution/fetch_content/test_nn.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include <nn/endian.h>
+
+int main() {
+    uint16_t foo = 42;
+    uint16_t bar = nn_htons(foo);
+    printf("%i converted from host byte order to network byte order is %i\n", foo, bar);
+}
