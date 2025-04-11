533f438f7976dece5624e1905cb51519d31c6438
Thomas <tfk@debian>
2025-04-11 14:47:02 +0200

    add nn_strcat

--- components/nn/include/nn/string.h
+++ components/nn/include/nn/string.h
@@ -2,10 +2,26 @@
 #define _NN_STRING_H_
 
 #include <nn/allocator.h>
+#include <stdbool.h>
 
 /**
  * like strdup, duplicate a null terminated c string.
  */
 char* nn_strdup(const char* str, struct nn_allocator* allocator);
+
+/**
+ * Append the src string to the destination string if enough space is available.
+ * The src string is only copied if the remaining space in dst has room for the string and a trailing NULL.
+ *
+ * dstLen must be the total allocated size of dst. This means this function will only succeed if strlen(dst)+strlen(src) < dstLen
+ *
+ * @param dst The string to append to
+ * @param dstLen The total allocated size of dst
+ * @param src The string to copy
+ * @return true on success
+ */
+bool nn_strcat(char* dst, size_t dstLen, const char* src);
+
+
 
 #endif
--- components/nn/src/nn/string.c
+++ components/nn/src/nn/string.c
@@ -10,3 +10,16 @@ char* nn_strdup(const char* str, struct nn_allocator* allocator)
     memcpy(dupBuf, str, len+1);
     return dupBuf;
 }
+
+bool nn_strcat(char* dst, size_t dstLen, const char* src)
+{
+    size_t srcLen = strlen(src);
+    size_t orgStrLen = strlen(dst);
+    if (dstLen > srcLen+orgStrLen) {
+        // We have space for the string in dst and the src string and a trailing NULL
+        memcpy(dst+orgStrLen, src, srcLen+1);
+        return true;
+    } else {
+        return false;
+    }
+}
